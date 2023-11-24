2cf0e5c6744db2379e879506df4d40ca22568154
primadologic <prince.adimado@afrilogicsolutions.com>
2023-11-24 22:15:08 +0000

    add dowload btn for resume

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,7 @@
 "use clent"
 import Image from "next/image"
 import Link from "next/link"
+import { BsDownload } from "react-icons/bs"
 
 // Image
 import profile from "../public/assets/profile-pic.jpg"
@@ -13,13 +14,19 @@ export default function Hero() {
         <>
             <div className=" md-768:px-12 px-12">
                 <div className="flex flex-col gap-y-16 justify-center items-center gap-3 sm-425:px-5 sm-430:px-1">
-                    <div className="flex flex-col gap-y-3">
+                    <div className="flex flex-col justify-center items-center gap-y-3">
                         <h1 className="text-black text-5xl text-center font-semibold break-words">Hi, I&apos;m <span className="!text-c-red">Prince Adimado</span></h1>
-                        <div className="text-center sm-425:text-center lg:px-[15rem]">
+                        <div className="text-center sm-425:text-center lg:px-[15rem] ">
                             <p className="text-txt-gray text-base  break-words">
                                 With <span className="bg-c-red text-white px-1">{ experience } years</span>  of experience in web development with knowledge in Backend and <span className="!text-c-red font-semibold">Frontend Development</span> {' '} 
                                 I&apos;m able to understand and colloborate with other developers and designers to build a good responsive product to the market.
                             </p>
+                        </div>
+                        <div className="">
+                            <Link href="https://drive.google.com/file/d/1Qyd2Mmq8YUCDRZ7eySJJBSPI_LTw-yWe/view?usp=sharing" target="_blank" className="flex items-center gap-3 justify-center flex-row hover:bg-gray-500 max-w-max hover:bg-opacity-30 p-2 rounded-md">
+                                View my Resume
+                                <BsDownload className="text-black text-base font-bold" />
+                            </Link>
                         </div>
                     </div>
 
