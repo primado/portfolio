66e2a7571bcae1d721cadeac29629cd65f3dead6
primado <primadoprincepa@gmail.com>
2024-01-02 20:29:35 +0000

    fix height issue with Typing Animation

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-"use clent"
+"use client"
 import Image from "next/image"
 import Link from "next/link"
 import { BsDownload } from "react-icons/bs"
@@ -15,16 +15,17 @@ export default function Hero() {
         <>
             <div className=" md-768:px-12 px-12">
                 <div className="flex flex-col gap-y-16 justify-center items-center gap-3 sm-425:px-5 sm-430:px-1">
-                    <div className="flex flex-col justify-center items-center gap-y-3">
-                        <h1 className="text-black text-5xl text-center font-semibold break-words">Hi, I&apos;m <span className="!text-c-red">
+                    <div className="flex flex-col justify-center items-center gap-y-3 sm-425:gap-y-3">
+                        <h1 className="text-black text-5xl text-center font-semibold break-words sm-320:text-3xl sm-320:border-2 sm-320:border-black sm-425:p-3 sm-425:w-full sm-425:h-[12rem] sm-320:h-[6rem]">Hi, I&apos;m <span className="!text-c-red">
                             <TypingAnimation 
                                 strings={['Prince Adimado', 'a Software Developer', 'Frontend Developer']}
                                 typeSpeed={30}
                                 backSpeed={30}
                                 loop={true}
                             />
-                        </span></h1>
-                        <div className="text-center sm-425:text-center lg:px-[15rem] ">
+                            </span>
+                        </h1>
+                        <div className="text-center sm-425:text-center lg:px-[15rem] s">
                             <p className="text-txt-gray text-base  break-words">
                                 With <span className="bg-c-red text-white px-1">{ experience } years</span>  of experience in web development with knowledge in Backend and <span className="!text-c-red font-semibold">Frontend Development</span> {' '} 
                                 I&apos;m able to understand and colloborate with other developers and designers to build a good responsive product to the market.
