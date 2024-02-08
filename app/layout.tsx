c82111508d5ae287005c8feec6d69de411a19d1c
primadologic <primadoprincepa@gmail.com>
2024-02-08 12:54:06 +0000

    change metadate description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Prince Adimado's Portfolio",
-  description: 'This website is the Portfolio website for Prince Adimado. Prince Adimado is a Software Developer.'
+  title: "Prince Adimado's Portfolio Website",
+  description: 'Portfolio Website of Prince Adimado,  a Software Developer.'
 
 }
 
