031e37bc13e85300e9e6131e3ccc0e246a863707
primado <primadoprincepa@gmail.com>
2024-10-30 10:20:06 +0000

    fix and add projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,7 +1,18 @@
 import Link from "next/link"
 import { projects } from "../data/dataProjects"
 import Image from "next/image"
-import { MdArrowRightAlt } from "react-icons/md";
+import default_img from "@/public/assets/man-countries.png"
+
+export type ProjectDTO = {
+    id: number,
+    name: string,
+    url?: string,
+    demo?: string,
+    source?: string | null | undefined,  // Allow undefined
+    description: string,
+    imagePath: string | undefined
+}
+
 
 export default function Projects() {
 
@@ -12,27 +23,45 @@ export default function Projects() {
                     <h2 className="text-black text-3xl text-center font-semibold">Completed Projects</h2>
                 </div>
                 
-                <div  className=" flex justify-center items-center">
+                <div  className=" flex flex-col gap-8 justify-center items-center">
                 
-                    {projects.map(({ id, name, url, description, imagePath }) => (
+                    {projects.map((data: ProjectDTO) => (
 
-                        <div key={id} className="flex flex-row justify-center gap-8 lg:max-w-5xl md-768:flex-col md-768:gap-0">
+                        <div key={data.id} className="flex flex-row justify-center gap-8 lg:max-w-5xl md-768:flex-col md-768:gap-0">
 
                             <Image 
                                 alt="Man Countries project"
-                                src={imagePath}
+                                src={data.imagePath || default_img}
                                 width={500}
                                 height={500}
                                 className="hover:origin-top-left hover:rotate-2 duration-700 cursor-pointer hover:skew-x-1 transition-all"
                             />
-                            <div className=" flex flex-col gap-3">
-                                <h3 className="text-black text-2xl font-medium sm-425:text-center">{name}</h3>
-                                <p className="text-[#8e9196] text-left sm-425:text-left">{description}</p>
-                                <Link href={url} target="_blank" className="text-c-red text-lg p-2 font-medium flex items-center hover:underline sm-425:justify-center">
-                                   <span>Full View</span> 
-                                   <MdArrowRightAlt className="w-10 h-10 text-2xl" />
+                            <div className=" flex flex-col gap-3 sm:w-[35rem] sm:mt-0 mt-2 w-full">
+                                <h3 className="text-black text-xl font-medium sm-425:text-left">{data.name}</h3>
+                                <p className="text-[#8e9196] text-left sm-425:text-left">{data.description}</p>
+                                <div className="flex flex-row gap-3">
+                                    {data.url && (
+                                    <Link href={data.url} target="_blank" className="text-c-red text-base px-2 py-1 bg-slate-300 rounded-md font-semibold border-white border flex items-center hover:underline sm-425:text-left">
+                                       <span>Full View</span> 
+                                       {/* <MdArrowRightAlt className="w-5 h-5 text-2xl" /> */}
+                                    </Link>
+                                    )}
+                                    {data.source && (
+                                        <Link href={data.source} target="_blank" className="text-c-red text-base px-1 py-2 bg-slate-300 rounded-md font-semibold border-white border flex items-center hover:underline sm-425:text-left">
+                                           <span>Source</span> 
+                                           {/* <MdArrowRightAlt className="w-5 h-5 text-2xl" /> */}
 
-                                </Link>
+                                        </Link>
+                                    )}
+                                    {data.demo && (
+                                        <Link href={data.demo} target="_blank" className="text-c-red text-base px-1 py-2 bg-slate-300 rounded-md font-semibold border-white border flex items-center hover:underline sm-425:text-left">
+                                           <span>Demo Video</span> 
+                                           {/* <MdArrowRightAlt className="w-5 h-5 text-2xl" /> */}
+
+                                        </Link>
+                                    )}
+                                </div>
+                               
                             </div> 
                         
                         </div>
--- data/dataProjects.tsx
+++ data/dataProjects.tsx
@@ -1,13 +1,32 @@
+import { ProjectDTO } from "@/components/projects"
 
 
-
-export const projects = [
+export const projects: ProjectDTO[] = [
     {
         'id': 1,
         'name': 'Countries Around the Globe',
         'url': "https://man-countries.netlify.app/",
+        'source': 'https://github.com/primado/man-countries',
         "description": "A project that calls the Countries REST API v2 to provide and display information to know more about any countries. The web app is very responsive on all screen size and it also comes with a theme switcher.",
         "imagePath": "/assets/man-countries.png"
 
-    }
+    },
+    {
+        'id': 2,
+        'name': 'Recipe Management System',
+        'source': "https://github.com/primado/recipe-web/",
+        "description": "I built a full stack Recipe Management System. I built the APIs in Django and integrated the APIs by building the web app using Nextjs.",
+        "imagePath": "/assets/recipeApp.png"
+
+    },
+    {
+        'id': 3,
+        'name': 'The Journal Blog',
+        'url': 'https://journal-blog-pa.vercel.app/',
+        'demo': 'https://www.linkedin.com/posts/primado_nextjs-next-developer-activity-7255989802112618497-IHAH?utm_source=share&utm_medium=member_desktop',
+        // 'source': "https://github.com/primado/recipe-web/",
+        "description": "A Nextjs blog built with Sanity CMS.",
+        "imagePath": "/assets/journal.png"
+
+    },
 ]
